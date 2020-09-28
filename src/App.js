218c0e7dfc096b2e99ced1371e5c1fcb85bc1ef0
Muhammad Hassan <mhassan258182211@gmail.com>
2020-09-28 10:39:10 +0500

    lastUpdated

--- src/App.js
+++ src/App.js
@@ -6,16 +6,10 @@ import React, {
   useEffect,
 } from "react";
 import "./App.css";
-
-import HomePage from "./component/HomePage";
-import About from "./component/About";
-import Contact from "./component/Contact";
-import NavBar from "./component/NavBar";
 import MyContext from "./component/context";
-import { Launch } from "./component/launch";
-
 import { Lines } from "react-preloaders";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./component/HomePage";
+
 
 function App() {
   useEffect(() => {
@@ -54,20 +48,13 @@ function App() {
         setLoading: setLoading,
         setDataCity: setDataCity,
       }}
-      >
-        <BrowserRouter>
-      <NavBar />
-      <div style={{margin:"0px auto" , textAlign:"center"}}>
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/contact/:slug" component={Launch} />
-            <Route path="/contact" component={Contact} />
-            <Route exact path="/" component={HomePage} />
-            <Route path="/" component={HomePage} />
-          </Switch>
-      </div>
-        </BrowserRouter>
-      <Lines animation="slide-left" customLoading={loading} />
+    >
+
+
+
+      {/* <Lines animation="slide-left" customLoading={loading} /> */}
+
+      <HomePage />
     </MyContext.Provider>
   );
 }
--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -1,14 +1,114 @@
-import React, { useState } from 'react';
-import { Bar, Line, Scatter, Polar } from 'react-chartjs-2';
-import MyContext from './context'
-import Launch from '../component/launch'
-
-
-
-import { Link, Outlet } from "react-router-dom";
-
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { TextField, Button } from '@material-ui/core';
+import MyContext from "../component/context";
+import iState from './state';
 
 export default function HomePage() {
+
+  const [hours, setHours] = React.useState(0);
+  const [minutes, setMinutes] = React.useState(0);
+  const [seconds, setSeconds] = React.useState(0);
+  const [counter, setCounter] = React.useState(1);
+
+  const [paragraph, setParagraph] = useState();
+  const [onStart, setOnStart] = useState(false);
+  const [flag, setFlag] = React.useState(true);
+  const [getWords, setGetWords] = React.useState();
+  // const [typed, setTyped] = React.useState(["Hassan "]);
+
+  const [firtTime, setFirstTime] = React.useState(true);
+
+
+
+  const handleChange = (event) => {
+
+    let typedWord = event.target.value.split(" ")
+
+    if (firtTime) {
+      document.getElementById("ParagraphId").innerHTML = " ";
+      getWords.forEach((element, index) => {
+        document.getElementById("ParagraphId").innerHTML += `<span id=${index} >${element}</span> `;
+      })
+      setFirstTime(false)
+    }
+
+    getWords.forEach((element, index) => {
+
+
+      if (event.key == " ") {
+
+
+        if (index < typedWord.length) {
+          console.log(element, typedWord[index])
+          document.getElementById(index).style.background = "none"
+          document.getElementById(typedWord.length).style.background = "#efefef"
+
+          if (element === typedWord[index]) {
+            document.getElementById(index).style.color = "Green"
+
+          }
+          else {
+            document.getElementById(index).style.color = "Red"
+
+          }
+
+        } else if(index > typedWord.length ){
+
+          document.getElementById(index).style.color = "black"
+          document.getElementById(index).style.background = "none"
+
+        }
+
+
+
+      }
+
+
+
+
+    });
+
+
+
+    if (flag) {
+      setInterval(() => {
+        setCounter(counter => {
+          setCounter(counter + 1)
+          setSeconds(parseInt(counter % 60))
+          setMinutes(parseInt(counter / 60))
+          setHours(parseInt(counter / 3600))
+        });
+
+      }, 1000);
+      setFlag(false)
+    }
+
+  };
+
+
+  function pad(n) {
+    return (n < 10 ? '0' : '') + n;
+  }
+
+
+  let genrateParagraph = () => {
+    setParagraph(iState[Math.floor(Math.random() * 11) + 1])
+  }
+
+  let changeValue = (e) => {
+    setParagraph(e.target.value)
+
+
+  }
+  let startHandle = () => {
+
+    setOnStart(true)
+    setGetWords(paragraph.split(" "))
+
+  }
+
 
   return (
     <MyContext.Consumer>
@@ -16,19 +116,71 @@ export default function HomePage() {
 
         return (
           <div className="homePage">
-            <ul style={{listStyleType:"none"}}>
-              {Object.entries(shoes).map(([slug, {name,img}]) => {
-                console.log(name)
-                return (
-                  <li key={slug}>
-                    <Link style={{textDecoration:"none",color:"white"}} to={`/contact/${slug}`}>
-                    <h1 >{name}</h1>
-                    <img src={img}  height={400} alt="No Internet Connection" width={600} style={{boxShadow: "0 0 5px #808080" ,borderRadius:"5px"}} />
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
+            {/* <div className="time"> */}
+            <div id="countdown">
+              <div id='tiles'><span>{pad(hours)}</span><span>{pad(minutes)}</span><span>{pad(seconds)}</span></div>
+              <div class="labels">
+                <li>Hours</li>
+                <li>Mins</li>
+                <li>Secs</li>
+              </div>
+            </div>
+            {/* </div> */}
+            <p id="para"></p>
+
+            <div className="header">
+              <Button variant="contained" color="primary" onClick={genrateParagraph} className="btn-genrateParagraph">
+                Genrate Paragraph
+              </Button>
+            </div>
+            <div className="MainBox">
+              <Paper elevation={3} className="textBox2">
+
+                {(onStart) ?
+                  <p className="paragraphfield" id="ParagraphId" >
+
+                    {paragraph}
+
+                  </p>
+                  :
+                  <TextField
+                    className="textfield"
+                    id="genrateParagraph"
+                    label="Typing"
+                    multiline
+                    rows={19}
+                    variant="outlined"
+                    value={paragraph}
+
+                    onChange={changeValue}
+
+
+                    size={40}
+                  />
+
+                }
+
+              </Paper>
+
+
+              <Paper elevation={3} className="textBox2" >
+
+                <TextField
+                  className="textfield"
+                  id=""
+                  label="Typing"
+                  multiline
+                  rows={19}
+                  // defaultValue="Default Value"
+                  variant="outlined"
+                  onKeyDown={handleChange}
+                  onClick={startHandle}
+
+                />
+
+              </Paper>
+
+            </div>
           </div>
         )
       }}
@@ -36,37 +188,3 @@ export default function HomePage() {
     </MyContext.Consumer >
   );
 }
-
-const shoes = {
-  "air-jordan-3-valor-blue": {
-    name: "VALOUR BLUE",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "jordan-mars-270-london": {
-    name: "JORDAN MARS 270 LONDON",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "air-jordan-1-zoom-racer-blue": {
-    name: "RACER BLUE",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  },
-   "air-jordan-3-valor-green": {
-    name: "VALOUR BLUE",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "jordan-mars-270-london-pink": {
-    name: "JORDAN MARS 270 LONDON",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "air-jordan-1-zoom-racer-red": {
-    name: "RACER BLUE",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  }
-  
-};
